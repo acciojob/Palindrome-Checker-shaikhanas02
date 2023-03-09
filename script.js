a5f840134580d381e0e3f250e2daa74d0f12e732
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-03-09 23:15:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+  var n=str.length;
+  for(let i=0;i<n/2;i++){
+    if(str.charAt(i)!=str.charAt(n-i-1) || (str.charAt(i)-'a')-(str.charAt(n-i-1)-'a')!=26 || (str.charAt(i)-'a')-(str.charAt(n-i-1)-'a')!=-26){
+      return false;
+    }
+  }
+  return true;
 }
 module.exports = palindrome
